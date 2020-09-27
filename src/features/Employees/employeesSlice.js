3e3717ae510897afcdccc976ebf705a82ae30af5
Veljko Blagojevic <veljko@geta.no>
2020-09-27 17:53:14 +0200

    Forgot to update slice

--- src/features/Employees/employeesSlice.js
+++ src/features/Employees/employeesSlice.js
@@ -1,17 +1,31 @@
-import { createSlice, createSelector } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { employees } from "lib/mock-data";
 
 export const slice = createSlice({
   name: "employees",
-  initialState: [],
+  initialState: {
+    data: [],
+    filteredOut: [],
+  },
   reducers: {
-    setEmployees: (_state, action) => {
-      return action.payload;
+    setEmployees: (state, action) => {
+      state.data = action.payload;
+    },
+    // TODO: Doesn't return them in the order they were created.
+    filterEmployees: (state, action) => {
+      const { value, checked } = action.payload;
+      const id = +value;
+      if (checked) {
+        const filtered = state.data.filter((t) => t.position.id === id);
+        state.filteredOut = [...state.filteredOut, ...filtered];
+      } else {
+        state.filteredOut = state.filteredOut.filter((t) => t.position.id !== id);
+      }
     },
   },
 });
 
-export const { setEmployees } = slice.actions;
+export const { setEmployees, filterEmployees } = slice.actions;
 
 // Thunk (redux-toolkit provides out of the box)
 export const fetchEmployees = () => (dispatch) => {
